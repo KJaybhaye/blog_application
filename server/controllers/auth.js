11ba6d2c9edde01741705a43fe2fa19e373d8a82
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-05 23:24:20 +0530

    Changes to auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -23,13 +23,14 @@ const login = async (req, res, next) => {
     console.log(login);
     try {
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(400).json("Wrong credentials!");
+        if(!user){return res.status(400).json("Wrong credentials!")}
+        // !user && res.status(400).json("Wrong credentials!");
     
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("Wrong credentials!");
+        if(!validated){return res.status(400).json("Wrong credentials!");}
     
         const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        return res.status(200).json(others);
       } catch (err) {
         res.status(500).json(err);
       }
