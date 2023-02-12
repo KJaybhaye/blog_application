e0e924ee2baa8f7a92222c3ffd036cf53f197bf3
KJaybhaye <krushnajaybhaye01@gmail.com>
2023-02-12 16:47:07 +0530

    added image model and code to upload images to mongoDB database

--- server/index.js
+++ server/index.js
@@ -10,14 +10,19 @@ require("express-async-errors")
 
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const fs = require("fs");
+const ImageModel = require("./models/Image");
+const cors = require("cors");
+
 
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-
+app.set("view engine", "ejs");
+app.use(cors());
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/images", express.static(path.join(__dirname, "/images")));
 
 app.use("/api/v1/users", users);
@@ -28,24 +33,65 @@ app.use("/api/v1/auth", auth);
 
 // app.use(notFound);
 // app.use(errorHandler);
-var storage = multer.diskStorage({
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, __dirname + '/images')      //you tell where to upload the files,
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, req.body.name)
+//   }
+// })
+
+// var upload = multer({storage: storage,
+//     onFileUploadStart: function (file) {
+//       console.log(file.originalname + ' is starting ...')
+//     },
+// });
+
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname + '/images')      //you tell where to upload the files,
+    cb(null, "images");
   },
   filename: function (req, file, cb) {
-    cb(null, req.body.name)
-  }
-})
+    cb(
+      null,
+      req.body.name
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
-var upload = multer({storage: storage,
-    onFileUploadStart: function (file) {
-      console.log(file.originalname + ' is starting ...')
-    },
+// app.post('/api/v1/upload', upload.single('file'), function (req, res, next) {
+//   res.send("image uploaded!");
+// })
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+  const obj = {
+      img: {
+          data: fs.readFileSync(path.join(__dirname + "/images/" + req.body.name)),
+          contentType: "image/jpeg"
+      }
+  }
+  const newImage = new ImageModel({
+      image: obj.img
+  });
+  newImage.save((err) => {
+      err ? console.log(err) : res.redirect("/");
+  });
 });
 
-app.post('/api/v1/upload', upload.single('file'), function (req, res, next) {
-  res.send("image uploaded!");
-})
+app.get("/api/v1/images/:id",  (req, res) => {
+  ImageModel.find({name: req.params.id}, (err, images) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send("An error occurred", err);
+    } else {
+        res.status(200).send(images);
+    }
+  });
+});
+
+
+
 const start = async () => {
     try{
         await connectDB(process.env.MONGO_URI);
